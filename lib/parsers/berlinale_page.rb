9d9ce545f616d543526f9ac0801bc9f72e4001be
Adamantish <sonsequence@hotmail.com>
2017-02-11 17:58:01 +0100

    FEAT&TEST: Can identify available films in page

--- lib/parsers/berlinale_page.rb
+++ lib/parsers/berlinale_page.rb
@@ -1,5 +1,24 @@
-module Parsers
-class BerlinalePage
+require 'nokogiri'
 
-end
+module Parsers
+  class BerlinalePage
+    def initialize(body)
+      @body = body
+    end
+
+    private
+
+    def document
+      Nokogiri::HTML(body)
+    end
+
+    def film_row_nodes
+      films = document.css('table.programmeTable > tbody > tr')
+      films.reject do |film|
+        film.css('a.sprite.tickets_N').empty?
+      end
+    end
+
+    attr_reader :body
+  end
 end
--- spec/lib/parsers/berlinale_page_spec.rb
+++ spec/lib/parsers/berlinale_page_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
+require_relative '../../../lib/scrapers/berlinale_programme'
 require_relative '../../../lib/parsers/berlinale_page'
 
 RSpec.describe Parsers::BerlinalePage do
+  let(:page_body) do
+    VCR.use_cassette('berlinale_page_20') do
+      Scrapers::BerlinaleProgramme.new(20).data
+    end
+  end
 
+  subject { described_class.new(page_body) }
+  describe '#film_row_nodes' do
+    it 'returns html_nodes with available tickets' do
+      subject.send(:film_row_nodes).each do |row|
+        buy_link = row.css('a.sprite.tickets_N').first.attributes['href'].value
+        expect(buy_link).to include('eventim.de')
+      end
+    end
+  end
 end
-
