2af3b4ffda4945598747a9144dd3bae75c312ae2
Adamantish <sonsequence@hotmail.com>
2017-02-11 19:07:05 +0100

    FEAT: Can offset to next row for key info

--- lib/parsers/berlinale_page.rb
+++ lib/parsers/berlinale_page.rb
@@ -6,17 +6,39 @@ module Parsers
       @body = body
     end
 
+    def films
+      title_row_nodes.map do |row_node|
+        title_and_link(row_node)
+      end
+    end
+
     private
 
     def document
       Nokogiri::HTML(body)
     end
 
-    def film_row_nodes
-      films = document.css('table.programmeTable > tbody > tr')
-      films.reject do |film|
-        film.css('a.sprite.tickets_N').empty?
+    def all_film_rows
+      @_all_film_rows ||= document.css('table.programmeTable > tbody > tr')
+    end
+
+    def key_row_indices
+      @_key_row_indices ||= [].tap do |indices|
+        all_film_rows.each_with_index do |film, i|
+          indices << i unless film.css('a.sprite.tickets_N').empty?
+        end
       end
+    end
+
+    def title_row_nodes
+      link_row_indices = key_row_indices.map { |i| i + 1 } # move to next row
+      all_film_rows.to_a.values_at(*link_row_indices)
+    end
+
+    def title_and_link(film_row_node)
+      link = film_row_node.css('a')[0]
+      { title: link.children.first.inner_html,
+        detail_path: link.attributes['href'].value }
     end
 
     attr_reader :body
--- spec/lib/parsers/berlinale_page_spec.rb
+++ spec/lib/parsers/berlinale_page_spec.rb
@@ -3,19 +3,29 @@ require_relative '../../../lib/scrapers/berlinale_programme'
 require_relative '../../../lib/parsers/berlinale_page'
 
 RSpec.describe Parsers::BerlinalePage do
+  subject { described_class.new(page_body) }
   let(:page_body) do
     VCR.use_cassette('berlinale_page_20') do
       Scrapers::BerlinaleProgramme.new(20).data
     end
   end
 
-  subject { described_class.new(page_body) }
-  describe '#film_row_nodes' do
+  describe '#key_row_indices' do
     it 'returns html_nodes with available tickets' do
-      subject.send(:film_row_nodes).each do |row|
-        buy_link = row.css('a.sprite.tickets_N').first.attributes['href'].value
+      rows = subject.send(:all_film_rows)
+      subject.send(:key_row_indices).each do |i|
+        buy_link = rows[i].css('a.sprite.tickets_N').first.attributes['href'].value
         expect(buy_link).to include('eventim.de')
       end
+    end
+  end
+
+  describe '#title_link' do
+
+  end
+
+  describe '#films' do
+    it 'returns array of hashes with relevant information' do
     end
   end
 end
