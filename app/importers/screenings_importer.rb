a9b96d1b3dc8998395c521becf0e7e0d6501e194
Adamantish <miss.rahee@gmail.com>
2018-02-13 18:29:11 +0100

    Try fixing importer to maintain soldout data

--- app/importers/screenings_importer.rb
+++ app/importers/screenings_importer.rb
@@ -27,8 +27,8 @@ class ScreeningsImporter
 
   def get_the_previous
     @the_previous ||= {}.tap do |hsh|
-      Screening.where(ticket_status: ['current', 'future']).select(:film_id, :cinema, :starts_at, :ticket_status, :sale_began_at).to_a.each do |screening|
-        hsh[screening.identifier] = { ticket_status_was: screening.ticket_status, sale_began_at_was: screening.sale_began_at }
+      Screening.where(ticket_status: ['current', 'future']).select(:film_id, :cinema, :starts_at, :ticket_status, :sale_began_at, :soldout_at).to_a.each do |screening|
+        hsh[screening.identifier] = { ticket_status_was: screening.ticket_status, sale_began_at_was: screening.sale_began_at, soldout_at_was: screening.soldout_at }
       end
     end
   end
@@ -37,11 +37,12 @@ class ScreeningsImporter
     Screening.new(screening_hash.merge(film_id: film.id)).tap do |screening|
       previous_details = @the_previous[screening.identifier] || {}
       screening.sale_began_at = previous_details[:sale_began_at_was]
+      screening.soldout_at = previous_details[:soldout_at_was]
       ticket_status_was       = previous_details[:ticket_status_was]
 
       transition = [ticket_status_was, screening.ticket_status]
       screening.sale_began_at ||= Time.now.utc if transition == ['future', 'current'] || transition == [nil, 'current']
-      screening.soldout_at = Time.now.utc      if transition == ['current', 'soldout']
+      screening.soldout_at    ||= Time.now.utc      if transition == ['current', 'soldout']
     end
   end
 end
