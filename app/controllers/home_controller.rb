0f42656ad0b0d26aeceecfda2ed42281d5886dde
Adamantish <miss.rahee@gmail.com>
2018-02-13 15:06:00 +0100

    Future film summary

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   include ApplicationHelper
   def index
     @screenings = filtered_scope
-    @film_count = @screenings.uniq.count(:page_url)
+    @future_screening_count = Screening.future.count
+    @future_film_count = Screening.future.select(:film_id).uniq.count
     @date_groups = @screenings.to_a.group_by(&:date_heading)
     begin
       count = @screenings.uniq.count(:page_url)
