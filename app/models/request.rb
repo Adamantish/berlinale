0fa2ab11fd6707659824ea641d88f49666380f24
Adamantish <miss.rahee@gmail.com>
2018-02-17 14:34:21 +0100

    Blacklist my IPs

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ActiveRecord::Base
+  IGNORE_IPS = ["79.192.212.131", "84.140.144.178", '109.45.1.55']
+
   enum kind: [:visitor, :refresh]
-  default_scope { where.not(remote_ip: '109.45.1.55') }
+  default_scope { where.not(remote_ip: IGNORE_IPS) }
 end
